68c7eafd059f3f660eb500a700daed6e2ffce550
Thiago-Cruz-Eng <th24093@gmail.com>
2024-01-28 21:06:07 -0300

    wip chessboard

---
+++ src/Api.ts
@@ -0,0 +1,24 @@
+// api.ts
+import axios, { AxiosRequestConfig} from 'axios';
+
+interface ApiResponse<T> {
+    data: T;
+}
+
+const baseURL = 'https://api.example.com'; // Replace with your API base URL
+
+const axiosInstance = axios.create({
+    baseURL,
+    timeout: 5000, // Adjust as needed
+    headers: {
+        'Content-Type': 'application/json',
+        // You can add common headers here if needed
+    },
+});
+
+export const api = {
+    get: <T>(url: string, config?: AxiosRequestConfig) => axiosInstance.get<ApiResponse<T>>(url, config),
+    post: <T>(url: string, data?: any, config?: AxiosRequestConfig) => axiosInstance.post<ApiResponse<T>>(url, data, config),
+    put: <T>(url: string, data?: any, config?: AxiosRequestConfig) => axiosInstance.put<ApiResponse<T>>(url, data, config),
+    delete: <T>(url: string, config?: AxiosRequestConfig) => axiosInstance.delete<ApiResponse<T>>(url, config),
+};
